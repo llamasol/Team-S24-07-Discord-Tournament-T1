e199aa52575e6073d34f7ff2cadf2b25baef5c1a
Lily Lama-Solet <guillaume@lama-solet.com>
2024-01-31 21:25:01 -0500

    Updated code to hide discord bot token in a local file that is stored on host's system

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
-
+import os
+from dotenv import load_dotenv, find_dotenv
 
 """
 This section defines the intents that the bot will use
@@ -8,22 +9,29 @@ as well as logging into the bot and returning to the console which bot was logge
 When you run the client via the command line, you should see the following output:
 Logged in as <insert bot name>
 """
+
+load_dotenv(find_dotenv())
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 owner = 97355249395716096
+token = os.getenv('TOKEN')
+
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
 
+
+#little connection test message response system
 @client.event
 async def on_message(message):
     if message.author == client.user and message.content.startswith('ping'):
         await message.channel.send('Why are you signed into the bot on discord...')
+        return
     if message.author.id == owner and message.content.startswith('ping'):
         await message.channel.send('pong')
         return 'owner said ping'
 
-#Client run command, hide token in a seperate config file that will not be uploaded to github
-client.run('MTIwMTcwNjUwNTQ4MTc0ODUxMQ.GUrx0H.Q8riBO8Cv9fn9DcKf-Sb1j1KZJCcBMY-BjxNrw')
+#starts the bot
+client.run(token)
